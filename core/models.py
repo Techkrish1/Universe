7681e4a07d256fbaec3047b507dd515bd77727f5
gokul <ghasy79>
2023-08-16 20:29:42 +0530

    first commit

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# user authentication
+# class Profile(models.Model):
+#     user = pass
+#     id_user = pass
+#     bio = pass
+#     profileimg = models.ImageField()
+#     location = pass
