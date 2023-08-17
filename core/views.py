ddf36dd50e83d67246dc9a098c7049a0dd9e86e9
gokul <ghasy79>
2023-08-17 21:07:13 +0530

    implementing posts

--- core/views.py
+++ core/views.py
@@ -10,7 +10,9 @@ from .models import Profile
 @login_required(login_url='signin')
 
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
 
 # User Authentication
 
@@ -105,3 +107,8 @@ def settings(request):
         return redirect('settings')
 
     return render(request, 'setting.html', {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def upload(request):
+    return HttpResponse('<h1>Hello</h1>')
