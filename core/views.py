dd9165c8be1d1de12e30ce6f7f620331c7300bb2
gokul <ghasy79>
2023-08-19 15:03:43 +0530

    if you follow any user then only you can see their posts

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages # It is used to sent messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, LikePost, FollowersCount
+from itertools import chain
 
 # Landing Page
 
@@ -13,8 +14,22 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
+    user_following_list = []
+    feed = []
+
+    user_following = FollowersCount.objects.filter(follower= request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+        feed.append(feed_lists)
+
+    feed_list = list(chain(*feed))
+
     posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_lists})
 
 
 # User Authentication
@@ -170,7 +185,7 @@ def profile(request, pk):
         button_text = 'unfollow'
     else:
         button_text = 'Follow'
-        
+
     user_followers = len(FollowersCount.objects.filter(user = pk))
     user_following = len(FollowersCount.objects.filter(follower = pk))
 
